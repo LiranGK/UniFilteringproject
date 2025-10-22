fd4620a33b63f79d453e4549d4e5ca551963ad6f
User <User@DESKTOP-1DC27OV>
2025-10-22 16:51:04 +0300

    Add project files.

---
+++ UniFilteringproject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniFilteringproject.Models;
+namespace UniFilteringproject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet<TEntity> properties here, for example:
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
---
+++ UniFilteringproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniFilteringproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
