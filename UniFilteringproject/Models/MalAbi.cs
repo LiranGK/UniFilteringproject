99183849a5cab448de006840d1c6ef9bb3f353f5
LiranGK <lirangilday@gmail.com>
2025-11-27 10:06:32 +0200

    hhhh

---
+++ UniFilteringproject/Models/MalAbi.cs
@@ -0,0 +1,12 @@
+﻿namespace UniFilteringproject.Models
+{
+    public class MalAbi
+    {
+        public int Id { get; set; }
+        public int MalshabId { get; set; }
+        public Malshab? malshab { get; set; }
+        public int AbilityId { get; set; }
+        public Ability? Ability { get; set; }
+        public int AbiLevel { get; set; }
+    }
+}
