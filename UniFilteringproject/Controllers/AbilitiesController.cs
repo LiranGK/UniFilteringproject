8b054994dcf503754cde4726f2dec47e666bac45
LiranGK <lirangilday@gmail.com>
2026-06-05 05:57:06 +0300

    Update AbilitiesController.cs

--- UniFilteringproject/Controllers/AbilitiesController.cs
+++ UniFilteringproject/Controllers/AbilitiesController.cs
@@ -52,8 +52,6 @@ namespace UniFilteringproject.Controllers
         }
 
         // POST: Abilities/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description")] Ability ability)
@@ -84,8 +82,6 @@ namespace UniFilteringproject.Controllers
         }
 
         // POST: Abilities/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Ability ability)
